5bdaee8c50f3da210f02889f5a29a71f93a923ac
dmuthui <vyda2002us@gmail.com>
2023-02-14 14:11:17 +0300

    Adds the project structure to git hub

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Login from '../login/Login';
+
+function App() {
+  return (
+    <React.Fragment>
+      <p> App component! </p>
+      <p>Para One</p>
+      <p>Para two</p>
+      <Login />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/login/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Login(){
+    return(
+        <React.Fragment>
+        <p class ='login.para'>Login Component!</p>
+        </React.Fragment>
+    )
+}
+export default Login;
